0d63c8fa5fecafd3d3acf764583c5f43c056cdde
philippkatterbach <k@tterbach.de>
2022-11-10 16:48:27 +0100

    Added Jupyter Notebook for executing

--- lab1/problem1/main.py
+++ lab1/problem1/main.py
@@ -1,14 +1,17 @@
 from maze import dynamic_programming
 from maze import draw_maze
+from maze import Maze
+from maze import animate_solution
 import numpy as np
 
 
 T = 20
 maze = [[0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 1, 0, 0], [0, 0, 1, 0, 0, 1, 1, 1], [0, 0, 1, 0, 0, 1, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 1, 0, 0, 0]]
-start = (0, 0)
+start = (0,0)
 maze = np.array(maze)
+env = Maze(maze)
 
-V, policy = dynamic_programming(maze, 20)
-path = maze.simulate(start, policy, 'DynProg')
-draw_maze(path)
+V, policy = dynamic_programming(env, 20)
+path = env.simulate(start, policy, 'DynProg')
+animate_solution(maze, path)
