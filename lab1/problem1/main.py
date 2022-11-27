153f26e36b7be2521a219c2019e7f597166c0f8d
philippkatterbach <k@tterbach.de>
2022-11-27 16:15:49 +0100

    version to pull for falco

--- lab1/problem1/main.py
+++ lab1/problem1/main.py
@@ -2,6 +2,7 @@ from maze import dynamic_programming
 from maze import draw_maze
 from maze import Maze
 from maze import animate_solution
+from maze import value_iteration
 import numpy as np
 
 
@@ -10,8 +11,18 @@ maze = [[0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 1, 0, 0], [0, 0, 1, 0, 0, 1, 1
         [0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 1, 2, 0, 0]]
 start = (0,0,6,5)
 maze = np.array(maze)
-env = Maze(maze)
 
-V, policy = dynamic_programming(env, 20)
-path = env.simulate(start, policy, 'DynProg')
+
+# define values for value iteration
+gamma = 0.99
+epsilon = 0.1
+
+# probability for geometric distribution with mean = 30
+p = 1/30
+
+# create environment
+env = Maze(maze, poison=True)
+V, policy = value_iteration(env, gamma, epsilon)
+
+path = env.simulate(start, policy, 'ValIter')
 animate_solution(maze, path)
